44f7d93f51cc240d805aa0c6c7bfcde2ae7b1a43
peache2 <eaguilar@elpuig.xeill.net>
2026-05-10 21:41:36 +0200

    2boss

---
+++ Scenes/boss_2.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@onready var player = get_tree().get_first_node_in_group("Player")
+@onready var animated_sprite = $AnimatedSprite2D
+
+var direction : Vector2
+var puede_hacer_daño : bool = false
+
+func _ready():
+	if not player:
+		player = get_tree().get_first_node_in_group("Player")
+	
+	await get_tree().create_timer(0.5).timeout
+	puede_hacer_daño = true
+
+func _process(_delta):
+	if player:
+		direction = player.global_position - global_position
+		animated_sprite.flip_h = direction.x < 0
+
+func _physics_process(_delta):
+	if player:
+		velocity = direction.normalized() * 40
+		move_and_slide()
+
+
+
+func _on_damage_player_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		if GameManager:
+			GameManager.take_damage(0.5)
+			$HitSound.play()
+		
+		
